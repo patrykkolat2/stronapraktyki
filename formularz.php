6eab42534971c104b45d93f0fc8d1bb8697af7d6
patrykkolat2 <patryk.kolat@icloud.com>
2025-01-21 17:11:15 +0100

    ss

--- formularz.php
+++ formularz.php
@@ -6,7 +6,7 @@ if(isset($_POST['wyslij']))
      $nazwisko = $_POST['nazwisko'];
      $email = $_POST['email'];
      $tresc = $_POST['tresc'];
-     $sql = "INSERT INTO users (imie,nazwisko,email,tresc)
+     $sql = "INSERT INTO wiadomosci (imie,nazwisko,email,tresc)
      VALUES ('$imie','$nazwisko','$email', '$tresc')";
      if (mysqli_query($con, $sql)) {
         print "Wiadomość wysłana!";
